e4cfbf50288508b5c3fd94d5325c3706837d0989
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-08-10 08:44:08 -0700

    updated

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+      
+    </div>
+  )
+}
+
+export default Navbar
