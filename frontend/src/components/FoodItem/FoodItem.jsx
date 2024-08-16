f7a1ac62aa7bc162181234e001ff8aef24d06771
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-08-16 13:10:50 -0700

    create footer

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
