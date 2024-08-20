8f6fee3afb3c9d7c0c7ae953991e5fd2c61552b6
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-08-20 12:18:25 -0700

    create loginpopup page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (setShowLogin) => {
 
     const [menu,setMenu] = useState("menu");
 
@@ -22,7 +23,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="" />
             <div className="dot"></div>
             <div>
-              <button>sign in</button>
+              <button onClick={()=>setShowLogin(true)}>sign in</button>
             </div>
           </div>
         </div>
