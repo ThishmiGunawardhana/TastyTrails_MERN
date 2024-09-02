7724131ce3c36d2e0f4d97aa13dc2df266d78e25
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-09-01 17:23:17 -0700

    fixed errors

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,36 +1,50 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useContext } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
-const FoodItem = ({id,name,price,description,image}) => {
-
-    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+// eslint-disable-next-line react/prop-types
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={image} alt="" />
-            {cartItems[id]
-                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
-                :<div className='food-item-counter'>
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-                </div>
-            }
+      <div className="food-item-img-container">
+        <img className='food-item-image' src={image} alt={name} />
+        {cartItems[id] > 0 ? (
+          <div className='food-item-counter'>
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt="Remove"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt="Add"
+            />
+          </div>
+        ) : (
+          <img
+            className='add'
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt="Add"
+          />
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_stars} alt="Rating" /> {/* Corrected typo */}
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
-            </div>
-            <p className='food-item-desc'>{description}</p>
-            <p className='food-item-price'>${price}</p>
-        </div>
+        <p className='food-item-desc'>{description}</p>
+        <p className='food-item-price'>${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,13 +1,46 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+
+    const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
+    return (
+        <div className='cart'>
+            <div className="cart-items">
+                <div className="cart-items-title">
+                    <p>Items</p>
+                    <p>Title</p>
+                    <p>Price</p>
+                    <p>Quantity</p>
+                    <p>Total</p>
+                    <p>Remove</p>
+                </div>
+                <br />
+                <hr />
+                {food_list.map((item, index) => {
+                  if (cartItems[item._id] > 0) {
+                    return (
+                      <div key={item._id}>
+                        <div className='cart-items-title cart-items-item'>
+                          <img src={item.image} alt={item.name} />  
+                          <p>{item.name}</p>
+                          <p>${item.price}</p>
+                          <p>{cartItems[item._id]}</p>
+                          <p>${item.price*cartItems[item._id]}</p>
+                        </div>
+                        <hr />
+                      </div>
+                    );
+                  }
+                  return null; 
+                })}
+
+            </div>
+        </div>
+    )
 }
 
 export default Cart
